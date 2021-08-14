f1ea5fc07104187dc48159076eb4663744322c6a
Tanakorn Pisnupoomi <intaniger@gmail.com>
2021-08-15 00:11:40 +0700

    try to fix something

--- db/index.ts
+++ db/index.ts
@@ -18,7 +18,7 @@ const files = readdirSync("public/assets/cats").filter((name) =>
   /^[0-9]{2}\..*/.test(name)
 );
 
-const data: CatPost[] = files.map((fileName, index) => {
+let data: CatPost[] = files.map((fileName, index) => {
   return {
     author: authors[index],
     title: sentence(),
@@ -27,4 +27,6 @@ const data: CatPost[] = files.map((fileName, index) => {
 });
 
 export const readData = () => data;
-export const addData = (element: CatPost) => data.push(element);
+export const addData = (element: CatPost) => {
+  data = data.concat(element);
+};
