68de330bed4eeec7d190d4fa8b99cb996a037cd4
nnichar <nicharee.chl@student.mahidol.edu>
2021-08-14 20:42:06 +0700

    Update stat.tsx

--- pages/stat.tsx
+++ pages/stat.tsx
@@ -1,8 +1,76 @@
 import React from "react";
+import { NextPage } from "next";
+import { CatPost } from "./types/Cats";
+import Link from "next/link";
+import { Table, Row, Col, Button} from 'antd';
+import { HomeOutlined } from '@ant-design/icons';
 
-const Stat = () => (
-  <div>
-    <h1>This is a stat!</h1>
-  </div>
-);
+interface StatProps {
+  posts: CatPost[];
+}
+
+const Stat: NextPage<StatProps> = ({ posts }) => {
+      
+    const accumulatedData = posts.reduce(
+      (prevAcc, {author}) => ({
+        ...prevAcc,
+        [author]: author in prevAcc ? prevAcc[author] +1 : 1,
+      }),
+      {}
+    );
+      
+    const data = Object.entries(accumulatedData).map(
+      ([author, number], index) =>({
+          key: index,
+          name: author,
+          number,
+
+      })
+    );
+
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+        },
+        {
+            title: ' The number of images',
+            dataIndex: 'number',
+            defaultSortOrder: 'descend',
+            sorter: (a, b) => a.number - b.number, 
+        },
+    ]
+
+    return (
+      <div>
+            <br /><br />
+            <Table columns={columns} dataSource={data} />
+
+            <Row justify="center">
+                <Col >
+                    <Link href="/" passHref>
+                        <Button type="primary" size="large" shape="round" icon={<HomeOutlined/> }> 
+                            Back to Home Page 
+                        </Button>
+                    </Link>
+                </Col>
+            </Row>   
+      </div>
+    );
+  }
+  
+  Stat.getInitialProps = async ({ req }) : Promise<StatProps> => {
+    let host = "";
+    if (req != undefined) {
+      const {
+        headers: { host: hostHeader },
+      } = req;
+      host = hostHeader;
+    } else {
+      host = "localhost:3000";
+    }
+    const res = await fetch(`http://${host}/api/getCats`);
+    return { posts: await res.json() };
+};
+
 export default Stat;
