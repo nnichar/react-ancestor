444755b7f9fc24d9039e18e632ed64b9f34cd977
nnichar <nicharee.chl@student.mahidol.edu>
2021-08-14 20:42:46 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,10 @@ import { CatPost } from "./types/Cats";
 import { NextPage } from "next";
 import { useState } from "react";
 import CatCard from "../components/CatCard";
-import { Row, Col } from "antd";
+import Link from "next/link";
+import { Row, Col, Button } from "antd";
+import { BarChartOutlined } from "@ant-design/icons";
+import { PlusSquareOutlined } from "@ant-design/icons";
 
 interface HomeProps {
   posts: CatPost[];
@@ -28,8 +31,22 @@ const Home: NextPage<HomeProps> = ({ posts }) => {
             </Col>
           ))}
         </Row>
+        <div className=""></div>
       </main>
       <footer className={styles.footer}>
+
+        <Link href="/create" passHref>
+            <Button type="primary" size="large" style={{ margin: 100 }} shape="round" icon={<PlusSquareOutlined />}> 
+                Create New Card 
+            </Button>
+        </Link>
+
+        <Link href="/stat" passHref>
+            <Button type="primary" size="large" style={{ margin: 100 }} shape="round" icon={<BarChartOutlined />}> 
+                Stat Page 
+            </Button>
+        </Link>
+
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -45,14 +62,18 @@ const Home: NextPage<HomeProps> = ({ posts }) => {
   );
 };
 
-Home.getInitialProps = async ({
-  req: {
-    headers: { host },
-  },
-}): Promise<HomeProps> => {
-  console.log(host);
-  const res = await fetch(`http://${host}/api/getCats`);
-  return { posts: await res.json() };
+Home.getInitialProps = async ({ req }) : Promise<HomeProps> => {
+    let host = "";
+    if (req != undefined) {
+      const {
+        headers: { host: hostHeader },
+      } = req;
+      host = hostHeader;
+    } else {
+      host = "localhost:3000";
+    }
+    const res = await fetch(`http://${host}/api/getCats`);
+    return { posts: await res.json() };
 };
 
 export default Home;
